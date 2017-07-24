c82d7155cf9b3f20c677bd42ffc47a979093b74c
Kim Patel <Kimemerypatel@gmail.com>
2017-07-24 10:33:36 -0400

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create([{name: "Computers"}, {name:'Televisions'}, {name: 'Smart Phones'}, {name: 'Stereos'}, {name: 'Other'}])
+# rails db:seed; comment out above if add more so it doesn't run again
